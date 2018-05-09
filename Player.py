e52e4292564ca7a6c9aedd11f0ae03246b9cb632
MadScrewdriver <33846428+MadScrewdriver@users.noreply.github.com>
2018-05-09 18:07:53 +0100

    Player-right,left - anim

--- Player.py
+++ Player.py
@@ -18,6 +18,10 @@ class Rocket(object):
         print(self.player_size)
         self.pos = Vector2(self.size[0] / 2 - self.player_size.x / 2, self.size[1] * (5/6))
         self.last_matrix = self.size[0]
+        self.player = pygame.image.load("ap1.png")
+        self.player_left = pygame.image.load("ap2.png")
+        self.player_right = pygame.image.load("ap3.png")
+        self.pressed = pygame.key.get_pressed()
 
     def update(self):
         self.size = self.game.screen.get_size()
@@ -31,6 +35,7 @@ class Rocket(object):
             self.pos.x = (self.pos.x / self.last_matrix) * self.size[0]
 
         self.last_matrix = self.size[0]
+        self.pressed = pygame.key.get_pressed()
 
     def tick(self):
         # Input
@@ -57,10 +62,22 @@ class Rocket(object):
             self.touch_r = False
             self.touch_l = False
 
-        self.game.screen.blit(pygame.transform.scale(player,
-                                                     (int(self.player_size.x),
-                                                      int(self.player_size.y))),
-                                                     (int(self.pos.x),
-                                                      int(self.pos.y)))
-
+        if self.pressed[pygame.K_LEFT]:
+            self.game.screen.blit(pygame.transform.scale(self.player_left,
+                                                         (int(self.player_size.x),
+                                                          int(self.player_size.y))),
+                                                         (int(self.pos.x),
+                                                          int(self.pos.y)))
+        elif self.pressed[pygame.K_RIGHT]:
+            self.game.screen.blit(pygame.transform.scale(self.player_right,
+                                                         (int(self.player_size.x),
+                                                          int(self.player_size.y))),
+                                                         (int(self.pos.x),
+                                                          int(self.pos.y)))
+        else:
+            self.game.screen.blit(pygame.transform.scale(self.player,
+                                                         (int(self.player_size.x),
+                                                          int(self.player_size.y))),
+                                                        (int(self.pos.x),
+                                                         int(self.pos.y)))
 
